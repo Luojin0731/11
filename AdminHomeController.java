71e8b086dd40da82296551d24727d961434d9b1e
DESKTOP-JTTLRHF\金哥 <3130256149@qq.com>
2021-01-04 16:55:58 +0800

    提交二

--- AdminHomeController.java
+++ AdminHomeController.java
@@ -41,6 +41,7 @@ public class AdminHomeController extends BaseController {
         logger.info("获取管理员信息");
         Admin admin = adminService.get(null,Integer.parseInt(adminId.toString()));
         map.put("admin",admin);
+
 //222
         logger.info("获取统计信息");
         Integer productTotal = productService.getTotal(null,new Byte[]{0,2});
